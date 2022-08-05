64a50fb1c3a0636417b9ec08a1ef62e14698665e
linhcoder21 <103647677+linhcoder21@users.noreply.github.com>
2022-08-06 01:09:08 +0700

    UI
    -UI Screen
    -UI Login
    -UI Register

--- app/src/main/java/vn/fpoly/veganfood/activity/SplashActivity.java
+++ app/src/main/java/vn/fpoly/veganfood/activity/SplashActivity.java
@@ -1,16 +1,40 @@
 package vn.fpoly.veganfood.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import vn.fpoly.veganfood.R;
 
 public class SplashActivity extends AppCompatActivity {
-
+    TextView tvHello;
+    LottieAnimationView lottie;
+    ImageView img_Logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tvHello = findViewById(R.id.tvHello);
+        lottie = findViewById(R.id.lottie);
+        img_Logo = findViewById(R.id.img_Logo);
+
+        tvHello.animate().translationY(-400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+        img_Logo.animate().translationY(400).setDuration(2000).setStartDelay(2900);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(i);
+            }
+        },5000);
     }
 }
