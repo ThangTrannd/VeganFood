40c288291da70c386e3a6a1b76a0bb102603145d
linhlinh <103647677+linhcoder21@users.noreply.github.com>
2022-11-19 17:45:23 -0800

    aa
     FFFF

--- app/src/main/java/vn/fpoly/veganfood/activity/main/HomeFragment.java
+++ app/src/main/java/vn/fpoly/veganfood/activity/main/HomeFragment.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import vn.fpoly.veganfood.R;
@@ -15,7 +18,7 @@ import vn.fpoly.veganfood.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
+   private ConstraintLayout constrantLayoutEndown1;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -38,5 +41,24 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        constrantLayoutEndown1 = view.findViewById(R.id.constrantLayoutEndown1);
+        initListener();
+    }
+
+    private void initListener() {
+        constrantLayoutEndown1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 }
