8e8f156d7b8be1df2de9b3c0a8cd0b4a6870bc75
linhlinh <103647677+linhcoder21@users.noreply.github.com>
2022-11-30 23:46:06 -0800

    fix

--- app/src/main/java/vn/fpoly/veganfood/activity/login/CategoryActivity.java
+++ app/src/main/java/vn/fpoly/veganfood/activity/login/CategoryActivity.java
@@ -11,6 +11,6 @@ public class CategoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
+        setContentView(R.layout.layout_list_cate);
     }
 }
