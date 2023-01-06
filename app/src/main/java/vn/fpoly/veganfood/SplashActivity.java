27af6e59f61352798a8b5d9ef9d13e21a6999e21
linhlinh <103647677+linhcoder21@users.noreply.github.com>
2023-01-05 20:07:31 -0800

    fix home

--- app/src/main/java/vn/fpoly/veganfood/SplashActivity.java
+++ app/src/main/java/vn/fpoly/veganfood/SplashActivity.java
@@ -2,8 +2,14 @@ package vn.fpoly.veganfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import vn.fpoly.veganfood.home.activityhome.HomeActivity;
 
@@ -11,18 +17,38 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
-
+    TextView tvHello;
+    LottieAnimationView lottie;
+    ImageView img_Logo;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+
+
+        tvHello = findViewById(R.id.tvHello);
+        lottie = findViewById(R.id.lottie);
+        img_Logo = findViewById(R.id.logo);
+
+        tvHello.animate().translationY(-400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+       img_Logo.animate().translationY(350).setDuration(2000).setStartDelay(2900);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                 finish();
             }
-        }, 2000);
+        },5000);
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+//                finish();
+//            }
+//        }, 2000);
     }
 }
